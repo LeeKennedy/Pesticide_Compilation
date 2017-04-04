639b28a812fb4c2bcb009db3bca4f1cfb53bb292
LeeKennedy <mail@leekennedy.id.au>
2017-04-04 22:31:34 +1000

    Create FSANZ comparison script.

--- FSANZ.R
+++ FSANZ.R
@@ -8,6 +8,16 @@ library(dplyr)
 library(tidyr)
 library(ggplot2)
 
+# Functions --------------------------------------------------------------
+mgsub <- function(pattern, replacement, x, ...) {
+  n=length(pattern)
+  result = x
+  for (i in 1:n) {
+    result[grep(pattern[i], x, ignore.case = FALSE, ...)] = replacement[i]
+  }
+  return(result)
+}
+
 # Data Input -------------------------------------------------------------
 PEST06 <- read_excel("~/Documents/GitHub/pest_data/PEST06.xlsx", 
                                sheet = "List")
@@ -21,80 +31,65 @@ PEST08 <- read_excel("~/Documents/GitHub/pest_data/PEST08.xlsx",
 PEST08C6 <- read_excel("~/Documents/GitHub/pest_data/PEST08C6.xlsx", 
                      sheet = "List")
 
-Vietnam <- read_excel("~/Documents/GitHub/pest_data/Vietnam.xlsx", 
-                       sheet = "List")
-
-Indonesia <- read_excel("~/Documents/GitHub/pest_data/Indonesia.xlsx", 
-                      sheet = "List")
-
-VN_Sub <- read_excel("~/Documents/GitHub/pest_data/Vietnam_Sub.xlsx", 
-                     sheet = "List")
-
-AAA <- read_excel("~/Documents/GitHub/pest_data/AAA.xlsx", 
-                  sheet = "List")
-
-Coles <- read_excel("~/Documents/GitHub/pest_data/Coles.xlsx", 
+FSANZ <- read_excel("~/Documents/GitHub/pest_data/FSANZ_Nuts2.xlsx", 
                     sheet = "List")
 
-NMI <- read_excel("~/Documents/GitHub/pest_data/NMI.xlsx", 
-                    sheet = "List")
-
-ALS <- read_excel("~/Documents/GitHub/pest_data/ALS.xlsx", 
-                  sheet = "List")
-
-FSANZ <- read_excel("~/Documents/GitHub/pest_data/FSANZ.xlsx", 
-                  sheet = "List")
-
-Agrifood <- read_excel("~/Documents/GitHub/pest_data/Agrifood.xlsx", 
-                    sheet = "List")
-
-Freshtest <- read_excel("~/Documents/GitHub/pest_data/Freshtest.xlsx", 
-                       sheet = "List")
-
-Combined <- rbind(PEST06, PEST08, PEST08C6, Vietnam, Indonesia, VN_Sub, AAA, Coles, NMI, ALS, FSANZ, Freshtest)
+Combined <- rbind(PEST06, PEST08, PEST08C6, FSANZ)
 
 Combined$marker <- "X"
 
 Combined <- na.omit(Combined)
 
+write.csv(Combined, "nuts.csv")
+
 #### Vocabulary Input -----------------------------
 vocab <- read_excel("~/Documents/GitHub/Pesticide_Compilation/vocab.xlsx")
+vocab <- vocab[,-1]
 v <- nrow(vocab)
 
 # Data Cleaning ----------------------------------------------------------
+
+PestName <- Combined$Name
+
 
 m <- nrow(Combined)
 
 for (i in 1:m) {
 
   for (j in 1:v) {
-
     
-    pref <- as.list(vocab[j,1])
-  
     list <- (vocab[j,])
 
     list <- list[colSums(!is.na(list)) > 0]
+    
+    pref_rep <- length(list)-1
+    
+    pref <- rep((vocab[j,1]), pref_rep)
 
     n <- ncol(list)
     
     if (n==2){
       check_list <- list[1,2]
     } else if (n==3) {
-      check_list <- paste(list[1,2],"|",list[1,3], sep="") 
+      check_list <- c(list[1,2],list[1,3]) 
     } else if (n==4) {
-      check_list <- paste(list[1,2],"|",list[1,3],"|",list[1,4], sep="")
+      check_list <- c(list[1,2],list[1,3],list[1,4])
     } else if (n==5) {
-      check_list <- paste(list[1,2],"|",list[1,3],"|",list[1,4],"|",list[1,5], sep="")
+      check_list <- c(list[1,2],list[1,3],list[1,4],list[1,5])
+    } else if (n==6) {
+      check_list <- c(list[1,2],list[1,3],list[1,4],list[1,5], list[1,6])
     }
-  
-    Combined$Name[i] <- sub(check_list, pref, Combined$Name[i])  
+    
+    PestName[i] <- mgsub(check_list, pref, PestName[i])  
   }
 }
   
+PestName2 <- t(data.frame(PestName, stringsAsFactors = FALSE))
+
+Combined$Name <- PestName2[1:m]
 
 # Exporting Data -------------------------------------------------------
 
 Wide_Data <- spread(Combined, Screen, marker, fill="")
 
-write_csv(Wide_Data, "pest_set.csv")
+write_csv(Wide_Data, "nut_set2.csv")
